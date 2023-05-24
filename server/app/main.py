1a386d0d9a6fba0ad4942e58da4d262723d40298
Soh Ohara <sohoha@amazon.co.jp>
2023-05-24 11:26:10 +0900

    Fixed bugs

--- server/app/main.py
+++ server/app/main.py
@@ -9,12 +9,11 @@ from schemas import MessageBody
 
 app = FastAPI()
 
-origins = [os.environ["ALLOW_ORIGINS"].split(",")]
-
+origins = os.environ["ALLOW_ORIGINS"].split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
