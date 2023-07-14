5e0bbdb9b19137b6c0124603e735b2f183dbfdc3
Soh Ohara <sohoha@amazon.co.jp>
2023-07-14 14:48:16 +0900

    Added comment

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -8,41 +8,27 @@ from typing import List, Literal
 from pydantic import BaseModel, Field
 
 
-class QueryBody(BaseModel):
-    query: str
-    user_id: str
-    query_type: Literal["kendra", "llm"]
+class KendraDocument(BaseModel):
+    """Kendra の検索結果"""
 
-
-class RagQueryBody(BaseModel):
-    query: str
-    llm_type: Literal["rinna", "calm"] = Field(
-        "rinna", description="裏側の LLM として何を使うのか切り替える"
+    excerpt: str = Field(description="ドキュメントの中身からの抜粋。LLM へのプロンプトにはここの値が使われます。")
+    title: str = Field(description="ドキュメントのタイトル. QUESTION_ANSWER の場合は質問文がここに入る想定")
+    content: str = Field(description="ドキュメルの本文")
+    type: Literal["DOCUMENT", "QUESTION_ANSWER", "ANSWER"] = Field(
+        description="ドキュメントの種類"
     )
 
 
-class RinnaModelKwargs(BaseModel):
-    max_new_tokens: int = Field(128)
-    temperature: float = Field(0.3)
-    do_sample: bool = Field(True)
-    pad_token_id: int = Field(0)
-    bos_token_id: int = Field(2)
-    eos_token_id: int = Field(3)
-
-
-class KendraDocument(BaseModel):
-    excerpt: str
-    title: str = Field(description="ドキュメントのタイトル. QUESTION_ANSWER の場合は質問文がここに入る想定")
-    content: str
-    type: Literal["DOCUMENT", "QUESTION_ANSWER", "ANSWER"]
-
-
 class ChatHistory(BaseModel):
-    utterance: str
-    type: Literal["AI", "Human"]
+    """AI とユーザーとのやり取りの履歴"""
+
+    utterance: str = Field(description="ユーザーの発言内容")
+    type: Literal["AI", "Human"] = Field(description="発言主体が AI(LLM) か Human (ユーザー)か")
 
 
 class LLMWithDocReqBody(BaseModel):
+    """POST /llm-with-doc でのリクエストボディ"""
+
     userUtterance: str = Field(description="ユーザーの発言内容")
     history: List[ChatHistory] = Field(description="AI とユーザーとのやり取りの履歴")
-    documents: List[KendraDocument]
+    documents: List[KendraDocument] = Field(description="Kendra による検索結果一覧")
