f0a8d4f505a848003c7d7e0231fef61b275567a1
tanimiya <141223513+tanimiya@users.noreply.github.com>
2023-08-09 13:06:20 +0900

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
 fi
 
 index_name="$1"
-KendraIndexID=$(aws kendra list-indices --no-paginate | jq --arg name "$index_name" '.IndexConfigurationSummaryItems[] | select(.Name == $name) | .Id')
+KendraIndexID=$(aws kendra list-indices --no-paginate | jq -r --arg name "$index_name" '.IndexConfigurationSummaryItems[] | select(.Name == $name) | .Id') 
 echo $KendraIndexID >>amplify/backend/api/fargate/secrets/.secret-kendra
 cp amplify/backend/api/fargate/src/docker-compose-template.yml amplify/backend/api/fargate/src/docker-compose.yml
 VITE="VITE_INDEX_ID=${KendraIndexID}\n# VITE_SERVER_URL=http://localhost:8080"
