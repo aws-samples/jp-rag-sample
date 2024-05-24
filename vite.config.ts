2fdab56cc7c52ec42ec4e2f86dfbf00d484db933
Yuki Sekiya <ysekiy@amazon.co.jp>
2024-05-24 00:40:17 -0700

    disable console log output in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'aws': ['@aws-sdk/client-kendra'],
-          'chakra-ui': ['@chakra-ui/icons'],
-          '@chakra-ui/react': ['@chakra-ui/react']
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'aws': ['@aws-sdk/client-kendra'],
+            'chakra-ui': ['@chakra-ui/icons'],
+            '@chakra-ui/react': ['@chakra-ui/react']
+          },
         },
       },
     },
-  },
-  base: "./",
-  resolve: {
-    alias: [
-    {
-      find: './runtimeConfig',
-      replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+    esbuild: {
+      drop: command === 'build' ? ['console', 'debugger'] : [],
     },
-  ]
-}
+    base: "./",
+    resolve: {
+      alias: [
+        {
+          find: './runtimeConfig',
+          replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+        },
+      ]
+    }
+  }
 })
